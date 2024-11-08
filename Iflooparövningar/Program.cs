29f3f497520192e66cd724039e01e09408cfdefd
Vincent Sabrie <vincent.sabrie@elev.ga.ntig.se>
2024-11-08 10:50:43 +0100

    12345 password (övning 3)

--- Iflooparövningar/Program.cs
+++ Iflooparövningar/Program.cs
@@ -3,11 +3,21 @@
 // Console.WriteLine("Hello, World!");
 // }
 
-string name = Console.ReadLine();
+using System.ComponentModel;
 
+System.Console.WriteLine("What is your username???");
+string name = Console.ReadLine();
+string password = Console.ReadLine();
 if (name == "kalleanka")
 {
-    System.Console.WriteLine($"Welcome {name}!");
+    System.Console.WriteLine("What is the password??");
+    if (password == "12345")
+    {
+        System.Console.WriteLine($"Welcome {name}!");
+    }
+}
+else {
+ System.Console.WriteLine("Wrong username or password");
 }
 
 Console.ReadLine();
