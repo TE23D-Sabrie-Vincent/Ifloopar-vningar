7c7ec0389bd5e8932a52e945ad4cc533fd185001
Vincent Sabrie <vincent.sabrie@elev.ga.ntig.se>
2024-11-11 08:52:57 +0100

    For loop hello world övning 4

--- Iflooparövningar/Program.cs
+++ Iflooparövningar/Program.cs
@@ -3,21 +3,34 @@
 // Console.WriteLine("Hello, World!");
 // }
 
-using System.ComponentModel;
+// using System.ComponentModel;
 
-System.Console.WriteLine("What is your username???");
-string name = Console.ReadLine();
-string password = Console.ReadLine();
-if (name == "kalleanka")
-{
-    System.Console.WriteLine("What is the password??");
-    if (password == "12345")
-    {
-        System.Console.WriteLine($"Welcome {name}!");
-    }
+// System.Console.WriteLine("What is your username???");
+// string name = Console.ReadLine();
+// string password = Console.ReadLine();
+// if (name == "kalleanka")
+// {
+//     System.Console.WriteLine("What is the password??");
+//     if (password == "12345")
+//     {
+//         System.Console.WriteLine($"Welcome {name}!");
+//     }
+// }
+// else {
+//  System.Console.WriteLine("Wrong username or password");
+// }
+
+
+// int i = 0;
+// while (i < 32){
+//    System.Console.WriteLine("Hello World");
+//    i++;
+// }
+// // while loop
+
+for (int i=0; i < 32; i++ ){
+    System.Console.WriteLine("Hello World!");
 }
-else {
- System.Console.WriteLine("Wrong username or password");
-}
+//for loop
 
 Console.ReadLine();
